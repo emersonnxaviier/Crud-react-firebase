58a02f879a35bc11501c13f65795ebd886b288bd
Émerson Xavier <emersonnxaviier@gmail.com>
2020-09-20 20:28:23 -0300

    crud-firebase-firestore

--- crud-react-firebase/src/firebaseConfig.js
+++ crud-react-firebase/src/firebaseConfig.js
@@ -5,13 +5,7 @@ import 'firebase/firebase-firestore';
 
 // configuração de quando é criado o banco.
 const firebaseConfig = {
-    apiKey: "AIzaSyBqvmhwe2kR2NCMKQuesTabpP0LIFgCAZI",
-    authDomain: "crudreact-fb547.firebaseapp.com",
-    databaseURL: "https://crudreact-fb547.firebaseio.com",
-    projectId: "crudreact-fb547",
-    storageBucket: "crudreact-fb547.appspot.com",
-    messagingSenderId: "1057428385801",
-    appId: "1:1057428385801:web:8946ddb148ea744593c8d8"
+    // configuração do firebase aqui.
 };
 
 // processo de conexão do firebase com a aplicação.
